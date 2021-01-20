8bf7f80c46c57b83cb532fc61f0f759238350e9f
Christian Nicolaisen <cvn_vitting@hotmail.com>
2021-01-20 19:05:26 +0100

    inital commimt

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import GhostTodoComp from './GhostTodoComp.svelte';  
+</script>
+
+<main>
+  <GhostTodoComp></GhostTodoComp>
+</main>
---
+++ src/GhostTodoComp.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+</script>
+
+<h1>Hello</h1>
+
+
+<style lang="scss"></style>
