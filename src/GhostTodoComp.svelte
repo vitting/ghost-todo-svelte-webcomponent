c853dbcd08145ca0caf06aa4a445501d5470bac0
Christian Nicolaisen <cvn_vitting@hotmail.com>
2021-01-23 15:22:58 +0100

    refactoring

--- src/GhostTodoComp.svelte
+++ src/GhostTodoComp.svelte
@@ -4,21 +4,29 @@
   import cuid from "cuid";
   import type { TodoItem } from "./interfaces/todo_item.interface";
   import { createEventDispatcher, onMount } from "svelte";
-  import GhostTodoFormComp from "./GhostTodoFormComp.svelte";
-  import GhostTodoItemComp from "./GhostTodoItemComp.svelte";
   import TodoItemsStore from "./stores/TodoItems.store";
 
   const dispatch = createEventDispatcher();
 
-  export let initTodos: string = "";
+  export let inittodos: string = "";
+  let todoInput: string = "";
 
-  function handleSubmit(event: CustomEvent<TodoItem>) {
-    TodoItemsStore.update((items) => [...items, event.detail]);
+  function handleSubmit() {
+    if (todoInput) {
+      const newTodoItem: TodoItem = {
+        id: cuid(),
+        title: todoInput,
+        completed: false,
+      };
+
+      TodoItemsStore.update((items) => [...items, newTodoItem]);
+
+      todoInput = "";
+    }
   }
 
-  function handleComplete(event: CustomEvent<TodoItem>) {
-    const item = event.detail;
-
+  function handleComplete(item: TodoItem) {
+    item.completed = !item.completed;
     TodoItemsStore.update((items) => {
       return items.map((todo) => {
         if (todo.id === item.id) {
@@ -30,17 +38,15 @@
     });
   }
 
-  function handleRemoveItem(event: CustomEvent<TodoItem>) {
-    const item = event.detail;
-
+  function handleRemoveItem(item: TodoItem) {
     TodoItemsStore.update((items) => {
       return items.filter((todo) => todo.id !== item.id);
     });
   }
 
   onMount(() => {
-    if (initTodos) {
-      const todos: TodoItem[] = initTodos.split(",").map<TodoItem>((item) => {
+    if (inittodos) {
+      const todos: TodoItem[] = inittodos.split(",").map<TodoItem>((item) => {
         const newItem: TodoItem = {
           id: cuid(),
           title: item.trim(),
@@ -51,6 +57,8 @@
       });
 
       TodoItemsStore.set(todos);
+    } else {
+      TodoItemsStore.set([]);
     }
   });
 
@@ -63,16 +71,30 @@
 
 <main>
   <h2>Svelte Web Component</h2>
-  <GhostTodoFormComp on:submitTodo={handleSubmit} />
+  <form on:submit|preventDefault={handleSubmit}>
+    <div class="control">
+      <input type="text" bind:value={todoInput} />
+      <button type="submit">Add</button>
+    </div>
+  </form>
 
   {#if $TodoItemsStore?.length}
     <ul>
       {#each $TodoItemsStore as todo (todo.id)}
-        <GhostTodoItemComp
-          item={JSON.stringify(todo)}
-          on:changeTodoCompleted={handleComplete}
-          on:clickTodoRemove={handleRemoveItem}
-        />
+        <li>
+          <div class="container">
+            <button type="button" title="Remove" on:click={() => handleRemoveItem(todo)}>
+              ❌
+            </button>
+            <div class:completed={todo?.completed} class="item">
+              <span>{todo?.title}</span>
+              <input
+                type="checkbox"
+                on:change={(e) => handleComplete(todo)}
+              />
+            </div>
+          </div>
+        </li>
       {/each}
     </ul>
   {:else}
@@ -92,5 +114,57 @@
     list-style: none;
     padding: 0;
     width: 300px;
+  }
+
+  form {
+    .control {
+      width: 300px;
+      display: flex;
+      input {
+        flex: 1;
+        margin-right: 5px;
+      }
+    }
+
+    button {
+      background-color: #5454ce;
+      color: white;
+      border: none;
+      border-radius: 4px;
+      padding: 5px;
+      outline: lightgray;
+    }
+  }
+
+  li {
+    padding: 5px;
+    background-color: rgb(144, 146, 27);
+    color: white;
+    margin-bottom: 5px;
+    border-radius: 6px;
+  }
+
+  .container {
+    display: flex;
+    align-items: center;
+
+    .item {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      &.completed {
+        text-decoration: line-through #5454ce 2px;
+      }
+    }
+  }
+
+  button {
+    padding: 0;
+    margin-right: 5px;
+    border: none;
+    background-color: transparent;
+    display: inline-block;
   }
 </style>
